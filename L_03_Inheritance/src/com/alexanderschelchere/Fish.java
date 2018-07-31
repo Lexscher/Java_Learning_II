6b8c01b76c3017af1fc76f23859790d12e158a42
Alexander Schelchere <ascellie@gmail.com>
2018-07-31 08:14:53 -0400

    Inheritance part 2

--- L_03_Inheritance/src/com/alexanderschelchere/Fish.java
+++ L_03_Inheritance/src/com/alexanderschelchere/Fish.java
@@ -1,4 +1,33 @@
 package com.alexanderschelchere;
 
-public class Fish {
+public class Fish extends Animal{
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int brain, int body, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest() {
+
+    }
+
+    private void moveMuscles() {
+        System.out.println("Muscles are a-movin'");
+    }
+
+    private void moveBackFin() {
+        System.out.println("Back fin is a-flappin'");
+    }
+
+    public void swim(int speed) {
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+    }
 }
--- L_03_Inheritance/src/com/alexanderschelchere/Main.java
+++ L_03_Inheritance/src/com/alexanderschelchere/Main.java
@@ -8,6 +8,9 @@ public class Main {
         Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "Long silky");
         dog.eat();
         dog.walk();
-        //dog.run();
+        dog.run();
+
+        Fish fish = new Fish("Tuna", 1, 1, 3, 5, 6, 2, 3);
+        fish.swim(8);
     }
 }
