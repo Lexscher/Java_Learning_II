308485966e927bb8cb7c669b84be153f1d868512
Alexander Schelchere <ascellie@gmail.com>
2018-09-06 09:00:04 -0400

    Arrays II

--- L_09_Arrays/src/com/alexanderschelchere/Main.java
+++ L_09_Arrays/src/com/alexanderschelchere/Main.java
@@ -3,13 +3,16 @@ package com.alexanderschelchere;
 public class Main {
 
     public static void main(String[] args) {
-//        int[] myIntArr = new int[10];
-//        myIntArr[0] = 45;
-//        myIntArr[1] = 476
-//        myIntArr[5] = 50;
-//        The shorter way of doing this
-        int[] myIntArr = { 1,2,3,4,5,6,7,8,9,10 };
-        double[] myDoubleArr = new double[10];
-        System.out.println(myIntArr[5]);
+        int[] myIntArr = new int[25];
+
+        for (int i = 0; i < myIntArr.length; i++) {
+            myIntArr[i] = i * 10;
+        }
+        printArray(myIntArr);
+    }
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + "'s value is " + array[i]);
+        }
     }
 }
